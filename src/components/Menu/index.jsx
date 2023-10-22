2b534bcd2ec5debf412f37ead95a771803f55aa4
ykji <yvarshney44@gmail.com>
2023-10-22 23:30:58 +0530

    feat: add tooltip to menu icons

--- src/components/Menu/index.jsx
+++ src/components/Menu/index.jsx
@@ -1,12 +1,6 @@
 import { useDispatch, useSelector } from "react-redux";
 
-import {
-  faPencil,
-  faEraser,
-  faRotateLeft,
-  faRotateRight,
-  faFileArrowDown,
-} from "@fortawesome/free-solid-svg-icons";
+import { faPencil, faEraser, faRotateLeft, faRotateRight, faFileArrowDown } from "@fortawesome/free-solid-svg-icons";
 import cx from "classnames";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
@@ -30,6 +24,7 @@ const Menu = () => {
   return (
     <div className={styles.menuContainer}>
       <div
+        title="Pencil"
         className={cx(styles.iconWrapper, {
           [styles.active]: activeMenuItem === MENU_ITEMS.PENCIL,
         })}
@@ -38,6 +33,7 @@ const Menu = () => {
         <FontAwesomeIcon icon={faPencil} className={styles.icon} />
       </div>
       <div
+        title="Eraser"
         className={cx(styles.iconWrapper, {
           [styles.active]: activeMenuItem === MENU_ITEMS.ERASER,
         })}
@@ -45,22 +41,13 @@ const Menu = () => {
       >
         <FontAwesomeIcon icon={faEraser} className={styles.icon} />
       </div>
-      <div
-        className={styles.iconWrapper}
-        onClick={() => handleActionItemClick(MENU_ITEMS.UNDO)}
-      >
+      <div title="Undo" className={styles.iconWrapper} onClick={() => handleActionItemClick(MENU_ITEMS.UNDO)}>
         <FontAwesomeIcon icon={faRotateLeft} className={styles.icon} />
       </div>
-      <div
-        className={styles.iconWrapper}
-        onClick={() => handleActionItemClick(MENU_ITEMS.REDO)}
-      >
+      <div title="Redo" className={styles.iconWrapper} onClick={() => handleActionItemClick(MENU_ITEMS.REDO)}>
         <FontAwesomeIcon icon={faRotateRight} className={styles.icon} />
       </div>
-      <div
-        className={styles.iconWrapper}
-        onClick={() => handleActionItemClick(MENU_ITEMS.DOWNLOAD)}
-      >
+      <div title="Download" className={styles.iconWrapper} onClick={() => handleActionItemClick(MENU_ITEMS.DOWNLOAD)}>
         <FontAwesomeIcon icon={faFileArrowDown} className={styles.icon} />
       </div>
     </div>
