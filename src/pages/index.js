2fa127b6351fb5bc99c83b88e249c733f26cfa50
ykji <yvarshney44@gmail.com>
2023-10-23 00:03:59 +0530

    feat: add header

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,6 +5,7 @@ import Toolbox from "@/components/Toolbox";
 export default function Home() {
   return (
     <>
+      <h1 className="text-4xl font-bold m-5 absolute">Drawpad.</h1>
       <Menu />
       <Toolbox />
       <Board />
